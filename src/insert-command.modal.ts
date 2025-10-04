24648c151af40bbdcfe27dd4927ce76c17a3d986
Ulf Ninow <mail@fnumatic.de>
2025-10-04 17:20:04 +0200

    fix(types): fix type errors in insert command modal
    • Correct OptionsCollection key type from intersection to union
    • Update FuzzySuggestModal generic type for precise typing
    • Refine method parameter types for better type safety

--- src/insert-command.modal.ts
+++ src/insert-command.modal.ts
@@ -3,7 +3,7 @@ import DynamicTOCPlugin from "./main";
 import { ExternalMarkdownKey } from "./types";
 
 type OptionsCollection = Record<
-  Exclude<ExternalMarkdownKey, "None"> & "code-block",
+  Exclude<ExternalMarkdownKey, "None"> | "code-block",
   {
     label: string;
     value: string;
@@ -19,7 +19,7 @@ const options: OptionsCollection = {
   DevonThink: { label: "{{toc}}", value: "{{toc}}" },
   TheBrain: { label: "[/toc/]", value: "[/toc/]" },
 };
-export class InsertCommandModal extends FuzzySuggestModal<string> {
+export class InsertCommandModal extends FuzzySuggestModal<keyof OptionsCollection> {
   private plugin: DynamicTOCPlugin;
   callback: (item: string) => void;
   constructor(app: App, plugin: DynamicTOCPlugin) {
@@ -28,20 +28,19 @@ export class InsertCommandModal extends FuzzySuggestModal<string> {
     this.plugin = plugin;
     this.setPlaceholder("Type name of table of contents type...");
   }
-  getItems(): string[] {
+  getItems(): (keyof OptionsCollection)[] {
     if (this.plugin.settings.supportAllMatchers) {
-      return Object.keys(options);
+      return Object.keys(options) as (keyof OptionsCollection)[];
     }
     if (this.plugin.settings.externalStyle !== "None") {
-      return ["code-block", this.plugin.settings.externalStyle];
+      return ["code-block", this.plugin.settings.externalStyle as Exclude<ExternalMarkdownKey, "None">];
     }
     return ["code-block"];
   }
-  getItemText(id: string): string {
-    const foundKey = Object.keys(options).find((v) => v === id);
-    return options[foundKey].label;
+  getItemText(id: keyof OptionsCollection): string {
+    return options[id].label;
   }
-  onChooseItem(item: string): void {
+  onChooseItem(item: keyof OptionsCollection): void {
     this.callback(options[item].value);
   }
   public start(callback: (item: string) => void): void {
